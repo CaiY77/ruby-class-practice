3f79918dda4208c9c377b8387323727683cab229
Cai Yang <caih.yang@gmail.com>
2019-05-21 11:20:08 -0400

    inher

--- person.rb
+++ person.rb
@@ -1,7 +1,25 @@
 class Person
+# attr_reader :name, :age
+# attr_writer :name, :age
+attr_accessor :name, :age
+
+  def initialize(name: "Jesus",age:)
+    @name = name
+    @age = age
+  end
+
   def say_hi
-    puts "Hello World"
+    puts "Hi, my name is #{@name} and I am #{@age} years old"
+    yikes
+  end
+
+  private
+
+  def yikes
+    puts "I'm private"
   end
 end
 
-Person.new.say_hi
+
+first_p = Person.new(age:100 ,name:"Someone");
+first_p.say_hi
